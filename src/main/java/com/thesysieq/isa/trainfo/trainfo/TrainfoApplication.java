38d6e685c8f8597d45247e9e32fd8241d838292d
theSysieq <krzychubak2000@gmail.com>
2024-12-11 00:55:09 +0100

    Initial commit

---
+++ src/main/java/com/thesysieq/isa/trainfo/trainfo/TrainfoApplication.java
@@ -0,0 +1,13 @@
+package com.thesysieq.isa.trainfo.trainfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainfoApplication.class, args);
+	}
+
+}
