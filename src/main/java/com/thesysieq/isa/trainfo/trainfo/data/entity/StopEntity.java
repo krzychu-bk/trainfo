c4669d5b952ddfb081dd5150b09236f05a78a029
theSysieq <krzychubak2000@gmail.com>
2024-12-11 02:52:20 +0100

    Added all JPA entities' annotations

--- src/main/java/com/thesysieq/isa/trainfo/trainfo/data/entity/StopEntity.java
+++ src/main/java/com/thesysieq/isa/trainfo/trainfo/data/entity/StopEntity.java
@@ -3,19 +3,41 @@ package com.thesysieq.isa.trainfo.trainfo.data.entity;
 import java.io.Serializable;
 
 import com.thesysieq.isa.trainfo.trainfo.remote.rest.dto.StopDto;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
 import lombok.*;
 import java.sql.Time;
+import java.util.UUID;
 
+@Entity
+@Table(name="stops")
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 
 public class StopEntity implements Comparable<StopEntity>, Serializable {
+
+    @Id
+    @Column(name = "stop_id", nullable=false, unique=true, updatable=false)
+    private UUID stopID;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "train_id", nullable=false)
     @ToString.Exclude
     //@EqualsAndHashCode.Exclude
     private TrainEntity train;
+
+    @Column(name = "station", nullable = false)
     private String station;
+
+    @Column(name = "arrival", nullable = false)
     private Time arrival;
+
+    @Column(name = "departure", nullable = false)
     private Time departure;
+
+    @Column(name = "distance_from_origin_km", nullable = false)
     private Float distanceFromOriginKm;
 
     @Override
